0e1c18ff4662b35fa942028036f76fd36f691b36
alexeskenazi <112673600+alexeskenazi@users.noreply.github.com>
2024-10-17 21:32:29 -0400

    Bimodal two bit predictor

--- predictors.cpp
+++ predictors.cpp
@@ -8,6 +8,18 @@ using namespace std;
 
 #define MAX_TABLE_SIZE 4096
 
+#define NOT_TAKEN 0
+#define TAKEN 1
+
+  // 00: Strongly not taken
+  // 01: Weakly not taken
+  // 10: Weakly taken
+  // 11: Strongly taken
+#define STRONGLY_NOT_TAKEN 0
+#define WEAKLY_NOT_TAKEN 1
+#define WEAKLY_TAKEN 2
+#define STRONGLY_TAKEN 3
+
 // Always taken predictor
 int always_taken_predictor_correct = 0;
 int always_taken_predictor_branch_count = 0;
@@ -21,6 +33,91 @@ int bimodal_single_bit_correct = 0;
 int bimodal_single_bit_branch_count = 0;
 int bimodal_two_bit_correct = 0;
 int bimodal_two_bit_branch_count = 0;
+
+
+bool isBimodalSingleBitPredictionCorrect(int *bimodal_bit_table, int index, string behavior) {
+  if(bimodal_bit_table[index] == NOT_TAKEN && behavior == "NT") {
+    return true;
+  }
+  else if(bimodal_bit_table[index] == TAKEN && behavior == "T") {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+void updateBimodalSingleBitTable(int *bimodal_bit_table, int index, string behavior) {
+  if(behavior == "T") {
+    bimodal_bit_table[index] = TAKEN;
+  }
+  else {
+    bimodal_bit_table[index] = NOT_TAKEN;
+  }
+}
+
+
+int getBimodalTwoBitPrediction(int *bimodal_bit_table, int index) {
+  // 00: Strongly not taken
+  // 01: Weakly not taken
+  // 10: Weakly taken
+  // 11: Strongly taken
+  if(bimodal_bit_table[index] == 0 || bimodal_bit_table[index] == 1) {
+    return 0; // Not taken
+  }
+  else {
+    return 1; // Taken
+  }
+}
+
+bool isBimodalTwoBitPredictionCorrect(int *bimodal_bit_table, int index, string behavior) {
+  if(getBimodalTwoBitPrediction(bimodal_bit_table, index) == 0 && behavior == "NT") {
+    return true;
+  }
+  else if(getBimodalTwoBitPrediction(bimodal_bit_table, index) == 1 && behavior == "T") {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+void updateBimodalTwoBitTable(int *bimodal_bit_table, int index, string behavior) {
+  // 00: Strongly not taken
+  // 01: Weakly not taken
+  // 10: Weakly taken
+  // 11: Strongly taken
+  if(behavior == "T") {
+    if(bimodal_bit_table[index] == STRONGLY_NOT_TAKEN) { // 
+      bimodal_bit_table[index] = WEAKLY_NOT_TAKEN;
+    }
+    else if(bimodal_bit_table[index] == WEAKLY_NOT_TAKEN) {
+      bimodal_bit_table[index] = WEAKLY_TAKEN;
+    }
+    else if(bimodal_bit_table[index] == WEAKLY_TAKEN) {
+      bimodal_bit_table[index] = STRONGLY_TAKEN;
+    }
+    else if(bimodal_bit_table[index] == STRONGLY_TAKEN) {
+      bimodal_bit_table[index] =  STRONGLY_TAKEN;
+    }
+  }
+  
+  
+  if(behavior == "NT") {
+    if(bimodal_bit_table[index] == STRONGLY_NOT_TAKEN) {
+      bimodal_bit_table[index] =  STRONGLY_NOT_TAKEN;
+    }
+    else if(bimodal_bit_table[index] == WEAKLY_NOT_TAKEN) {
+      bimodal_bit_table[index] = STRONGLY_NOT_TAKEN;
+    }
+    else if(bimodal_bit_table[index] == WEAKLY_TAKEN) {
+      bimodal_bit_table[index] = WEAKLY_NOT_TAKEN;
+    }
+    else if(bimodal_bit_table[index] == STRONGLY_TAKEN) {
+      bimodal_bit_table[index] = WEAKLY_TAKEN;
+    }
+  }
+}
 
 
 int main(int argc, char *argv[]) {
@@ -54,27 +151,6 @@ int main(int argc, char *argv[]) {
   }
 
 
-  // // The following loop will read a line at a time
-  // while (getline(infile, line)) {
-  //   // Now we have to parse the line into its two pieces
-  //   stringstream s(line);
-  //   s >> std::hex >> addr >> behavior;
-
-  //   // Now we can output it to both file and console
-  //   // cout << addr;
-  //   // outfile << addr;
-  //   if (behavior == "T") {
-  //     always_taken_predictor_branch_count++;
-  //     // cout << " -> taken" << endl;
-  //     // outfile << " -> taken" << endl;
-  //   } else {
-  //     always_taken_predictor_branch_count++;
-  //     always_taken_predictor_correct++;
-  //     // cout << " -> not taken" << endl;
-  //     // outfile << " -> not taken" << endl;
-  //   }
-  // }
-
   ////////////////////////////////////////////
   // Always taken predictor
   while (infile >> std::hex >> addr >> behavior) {
@@ -107,21 +183,20 @@ int main(int argc, char *argv[]) {
 
   // Creat table of table_size entries and intialize to 0 meaning not taken
   int max_table_size = MAX_TABLE_SIZE;
-  int bimodal_single_bit_table[MAX_TABLE_SIZE] = {0};
+  int bimodal_bit_table[MAX_TABLE_SIZE] = {0};
 
   int sizes[7] = {4, 8, 32, 64, 256, 1024, 4096};
 
+  // Bimodal single bit predictor
   for(int size_index = 0; size_index <7; size_index++) {
 
       bimodal_single_bit_correct = 0;
       bimodal_single_bit_branch_count = 0;
-      bimodal_two_bit_correct = 0;
-      bimodal_two_bit_branch_count = 0;
     
       // Initialize the table
       int table_size = sizes[size_index];
       for(int i = 0; i < MAX_TABLE_SIZE; i++) {
-        bimodal_single_bit_table[i] = 0;
+        bimodal_bit_table[i] = 0;
       }
 
       infile.clear();
@@ -132,27 +207,58 @@ int main(int argc, char *argv[]) {
         // Get the index of the table
         int index = addr % table_size;
 
-        // cout << addr;
-        // outfile << addr;
-        // cout << " Index: " << index << endl;
-
-        // Check if the prediction is correct
-        if (bimodal_single_bit_table[index] == 0 && behavior == "NT") {
-          bimodal_single_bit_correct++;
-        } else if (bimodal_single_bit_table[index] == 1 && behavior == "T") {
+        if(isBimodalSingleBitPredictionCorrect(bimodal_bit_table, index, behavior)) {
           bimodal_single_bit_correct++;
         }
-        // Update the table
-        if (behavior == "T") {
-          bimodal_single_bit_table[index] = 1;
-        } else {
-          bimodal_single_bit_table[index] = 0;
-        }
+       
+        updateBimodalSingleBitTable(bimodal_bit_table, index, behavior);
 
       }
 
       outfile << bimodal_single_bit_correct << "," << bimodal_single_bit_branch_count << ";" ;
       cout    << bimodal_single_bit_correct << "," << bimodal_single_bit_branch_count << ";" ;
+
+      // we need to print a space after every output except the last one
+      if(size_index != 6) {
+        outfile << " ";
+        cout    << " ";
+      }
+
+  }
+  outfile << endl;
+  cout    << endl;
+
+
+  // Bimodal two bit predictor
+  for(int size_index = 0; size_index <7; size_index++) {
+
+      bimodal_two_bit_correct = 0;
+      bimodal_two_bit_branch_count = 0;
+    
+      // Initialize the table
+      int table_size = sizes[size_index];
+      for(int i = 0; i < MAX_TABLE_SIZE; i++) {
+        bimodal_bit_table[i] = 0;
+      }
+
+      infile.clear();
+      infile.seekg(0, ios::beg);
+      while (infile >> std::hex >> addr >> behavior) {
+        bimodal_two_bit_branch_count++;
+
+        // Get the index of the table
+        int index = addr % table_size;
+
+        if(isBimodalTwoBitPredictionCorrect(bimodal_bit_table, index, behavior)) {
+          bimodal_two_bit_correct++;
+        }
+       
+        updateBimodalTwoBitTable(bimodal_bit_table, index, behavior);
+
+      }
+
+      outfile << bimodal_two_bit_correct << "," << bimodal_two_bit_branch_count << ";" ;
+      cout    << bimodal_two_bit_correct << "," << bimodal_two_bit_branch_count << ";" ;
 
       // we need to print a space after every output except the last one
       if(size_index != 6) {
