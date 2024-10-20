95739b76c4e3539201b5a7eb07633b5f0ccd24c1
alexeskenazi <112673600+alexeskenazi@users.noreply.github.com>
2024-10-20 10:49:35 -0400

    script add sec.txt processing

--- run_all.sh
+++ run_all.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-SHOW_FILE_OUTPUT=false
-
 echo "Compiling the code..."
 make
 
-
-
-echo "Running the code with short_trace1.txt..."
+echo "short_trace1.txt..."
 ./predictors traces/short_trace1.txt short_trace1_output.txt
-
-if [ "$SHOW_FILE_OUTPUT" = true ]; then
-    echo "Contents of short_trace1_output.txt:"
-    cat short_trace1_output.txt
-    echo ""
-fi
 
 # Check if the files are the same
 if diff -bB correct_outputs/short1.txt short_trace1_output.txt > /dev/null; then
@@ -25,14 +15,8 @@ fi
 
 
 
-echo "Running the code with short_trace2.txt..."
+echo "short_trace2.txt..."
 ./predictors traces/short_trace2.txt short_trace2_output.txt
-
-if [ "$SHOW_FILE_OUTPUT" = true ]; then
-    echo "Contents of short_trace2_output.txt:"
-    cat short_trace2_output.txt
-    echo ""
-fi
 
 # Check if the files are the same
 if diff -bB correct_outputs/short2.txt short_trace2_output.txt > /dev/null; then
@@ -42,14 +26,8 @@ else
 fi
 
 
-echo "Running the code with short_trace3.txt..."
+echo "short_trace3.txt..."
 ./predictors traces/short_trace3.txt short_trace3_output.txt
-
-if [ "$SHOW_FILE_OUTPUT" = true ]; then
-    echo "Contents of short_trace3_output.txt:"
-    cat short_trace3_output.txt
-    echo ""
-fi
 
 # Check if the files are the same
 if diff -bB correct_outputs/short3.txt short_trace3_output.txt > /dev/null; then
@@ -60,14 +38,8 @@ fi
 
 
 
-echo "Running the code with long_trace1.txt..."
+echo "long_trace1.txt..."
 ./predictors traces/long_trace1.txt long_trace1_output.txt
-
-if [ "$SHOW_FILE_OUTPUT" = true ]; then
-    echo "Contents of long_trace1.txt:"
-    cat long_trace1.txt
-    echo ""
-fi
 
 # Check if the files are the same
 if diff -bB correct_outputs/long1.txt long_trace1_output.txt > /dev/null; then
@@ -78,14 +50,9 @@ fi
 
 
 
-echo "Running the code with long_trace2.txt..."
+echo "long_trace2.txt..."
 ./predictors traces/long_trace2.txt long_trace2_output.txt
 
-if [ "$SHOW_FILE_OUTPUT" = true ]; then
-    echo "Contents of long_trace2.txt:"
-    cat long_trace2.txt
-    echo ""
-fi
 # Check if the files are the same
 if diff -bB correct_outputs/long2.txt long_trace2_output.txt > /dev/null; then
     echo "SUCCESS: The output matches! for long_trace2_output"
@@ -95,18 +62,23 @@ fi
 
 
 
-echo "Running the code with long_trace3.txt..."
+echo "long_trace3.txt..."
 ./predictors traces/long_trace3.txt long_trace3_output.txt
-
-if [ "$SHOW_FILE_OUTPUT" = true ]; then
-    echo "Contents of long_trace3.txt:"
-    cat long_trace3.txt
-    echo ""
-fi
 
 # Check if the files are the same
 if diff -bB correct_outputs/long3.txt long_trace3_output.txt > /dev/null; then
     echo "SUCCESS: The output matches! for long_trace3_output"
 else
     echo "FAILURE!!!!!!!!: The output does not match! long_trace3_output"
+fi
+
+
+echo "sec.txt..."
+./predictors traces/sec_trace2.txt sec_trace2_output.txt
+
+# Check if the files are the same
+if diff -bB correct_outputs/sec.txt sec_trace2_output.txt > /dev/null; then
+    echo "SUCCESS: The output matches! for sec_trace2_output"
+else
+    echo "FAILURE!!!!!!!!: The output does not match! sec_trace2_output"
 fi
